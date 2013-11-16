3897286e556873c4073fbbabf3566aab0a1bd583
Jesse Collier <jessecollier@gmail.com>
2013-11-15 16:55:12 -0800

    Adding chef configuration

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'berkshelf'
+gem 'ohai'
+gem 'chef','~> 11.8.0'
+
---
+++ chef/config.rb
@@ -0,0 +1,4 @@
+base_dir "/srv/configs/chef/"
+file_cache_path base_dir + "cache/"
+cookbook_path base_dir + "cookbooks/"
+json_attribs base_dir + "node.json"
