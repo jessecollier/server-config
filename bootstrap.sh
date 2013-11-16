bf8a61a93f1689ea84a918da32c8c9faf94a0a4f
Jesse Collier <jessecollier@gmail.com>
2013-11-15 17:29:13 -0800

    install ruby 1.9 and make it default

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,23 @@ logger "[$$] [$0] upgrade system complete"
 
 # Install ruby+chef
 logger "[$$] [$0] install ruby+chef"
-apt-get install -y ruby1.9.3 build-essential wget
+sudo apt-get install ruby1.9.1 ruby1.9.1-dev \
+  rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
+  build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev
+
+sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
+         --slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz \
+                        /usr/share/man/man1/ruby1.9.1.1.gz \
+        --slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
+        --slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
+        --slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
+
+# choose your interpreter
+# changes symlinks for /usr/bin/ruby , /usr/bin/gem
+# /usr/bin/irb, /usr/bin/ri and man (1) ruby
+sudo update-alternatives --config ruby
+sudo update-alternatives --config gem
+
 gem update --no-rdoc --no-ri
 logger "[$$] [$0] ruby+chef installed"
 
